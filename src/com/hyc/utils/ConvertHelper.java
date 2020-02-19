508d16edeb7b2f31f6d71ebf824acdcf7bfb76e4
贺宇成 <heyucheng001@ke.com>
2020-02-19 15:59:26 +0800

    支持string-array翻译

--- src/com/hyc/utils/ConvertHelper.java
+++ src/com/hyc/utils/ConvertHelper.java
@@ -19,28 +19,41 @@ public class ConvertHelper {
 
     public XmlDocument convert(String targetCode) {
         XmlDocument document = file.getDocument();
-        if(document == null) return null;
+        if (document == null) return null;
         document = (XmlDocument) document.copy();
+        if (document == null || document.getFirstChild() == null) {
+            return document;
+        }
         Api api = RetrofitUtil.getInstance().create(Api.class);
-        if (document != null && document.getFirstChild() != null) {
-            XmlTag tag = document.getRootTag();
-            for (XmlTag subTag : tag.getSubTags()) {
+        XmlTag tag = document.getRootTag();
+        for (XmlTag subTag : tag.getSubTags()) {
 //                String name = subTag.getAttributeValue("name");
-                String value = subTag.getValue().getText();
-                try {
-                    Response<Result> response = api.loadResult(CommonUtil.getParams(value, targetCode)).execute();
-                    if (response != null && response.body() != null && response.body().getTranslation() != null && response.body().getTranslation().size() > 0) {
-                        String data = response.body().getTranslation().get(0);
-                        System.out.println("翻译成功: " + value + "  结果:" + data + "\n");
-                        subTag.getValue().setText(data);
-                    }
-                } catch (IOException e) {
-                    System.err.println(e.getMessage());
-                    e.printStackTrace();
+            if (subTag.getName().equals("string")) {
+                getTranslate(api, subTag, targetCode);
+            } else if (subTag.getName().equals("string-array")) {
+                for (XmlTag itemTag : subTag.getSubTags()) {
+                    getTranslate(api, itemTag, targetCode);
                 }
             }
+
         }
         return document;
+    }
+
+
+    public void getTranslate(Api api, XmlTag subTag, String targetCode) {
+        String value = subTag.getValue().getText();
+        try {
+            Response<Result> response = api.loadResult(CommonUtil.getParams(value, targetCode)).execute();
+            if (response != null && response.body() != null && response.body().getTranslation() != null && response.body().getTranslation().size() > 0) {
+                String data = response.body().getTranslation().get(0);
+                System.out.println("翻译成功: " + value + "  结果:" + data + "\n");
+                subTag.getValue().setText(data);
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 
